fce365be10e3af4685e1f3a01d4cbefa0a0c73df
Unix1 <unix1@live.com>
2013-09-24 08:30:54 -0700

    add get_indexes and get_saved_searches

--- src/splunkclient.erl
+++ src/splunkclient.erl
@@ -6,8 +6,9 @@
 -export([start/2, stop/1]).
 
 %% User functions
--export([start/0, login/0, login/1, get_jobs/0, get_jobs/1, oneshot_search/1,
-         oneshot_search/2]).
+-export([start/0, login/0, login/1, get_indexes/0, get_indexes/1, get_jobs/0,
+         get_jobs/1, get_saved_searches/0, get_saved_searches/1,
+         oneshot_search/1, oneshot_search/2]).
 
 %% ============================================================================
 %% Application callbacks
@@ -32,12 +33,32 @@ start() ->
     start_dependencies(),
     application:start(splunkclient).
 
+get_indexes() ->
+    get_indexes(splunkclient_conn_default).
+
+get_indexes(Connection) ->
+    {ok, Pid} = supervisor:start_child(splunkclient_service_sup, []),
+    Result = splunkclient_service:get_indexes(Pid, Connection),
+    supervisor:terminate_child(splunkclient_service_sup, Pid),
+    supervisor:delete_child(splunkclient_service_sup, Pid),
+    Result.
+
 get_jobs() ->
     get_jobs(splunkclient_conn_default).
 
 get_jobs(Connection) ->
     {ok, Pid} = supervisor:start_child(splunkclient_service_sup, []),
     Result = splunkclient_service:get_jobs(Pid, Connection),
+    supervisor:terminate_child(splunkclient_service_sup, Pid),
+    supervisor:delete_child(splunkclient_service_sup, Pid),
+    Result.
+
+get_saved_searches() ->
+    get_saved_searches(splunkclient_conn_default).
+
+get_saved_searches(Connection) ->
+    {ok, Pid} = supervisor:start_child(splunkclient_service_sup, []),
+    Result = splunkclient_service:get_saved_searches(Pid, Connection),
     supervisor:terminate_child(splunkclient_service_sup, Pid),
     supervisor:delete_child(splunkclient_service_sup, Pid),
     Result.
--- src/splunkclient_service.erl
+++ src/splunkclient_service.erl
@@ -8,7 +8,7 @@
          handle_info/2, terminate/2, code_change/3]).
 
 %% API
--export([get_jobs/2, oneshot_search/3]).
+-export([get_indexes/2, get_jobs/2, get_saved_searches/2, oneshot_search/3]).
 
 %% ============================================================================
 %% Supervision functions
@@ -31,8 +31,14 @@ init([]) ->
 %% API functions
 %% ============================================================================
 
+get_indexes(Name, Connection) ->
+    gen_server:call(Name, {get_indexes, Connection}).
+
 get_jobs(Name, Connection) ->
     gen_server:call(Name, {get_jobs, Connection}).
+
+get_saved_searches(Name, Connection) ->
+    gen_server:call(Name, {get_saved_searches, Connection}).
 
 oneshot_search(Name, Connection, SearchTerm) ->
     gen_server:call(Name, {oneshot_search, Connection, SearchTerm}).
@@ -41,6 +47,16 @@ oneshot_search(Name, Connection, SearchTerm) ->
 %% Server functions
 %% ============================================================================
 
+handle_call({get_indexes, ConnectionName}, _From, S) ->
+    C = splunkclient_login:get_connection(ConnectionName),
+    case libget_indexes(C) of
+        {ok, Results} ->
+            {reply, {ok, search_results, Results}, S};
+        {error, Reason} ->
+            {stop, error, Reason, S};
+        _Else ->
+            {stop, error, "Unknown error", S}
+    end;
 handle_call({get_jobs, ConnectionName}, _From, S) ->
     C = splunkclient_login:get_connection(ConnectionName),
     case libget_jobs(C) of
@@ -50,6 +66,16 @@ handle_call({get_jobs, ConnectionName}, _From, S) ->
             {stop, error, Reason, S};
         _Else ->
             {stop, error, "Unknown error", S}
+    end;
+handle_call({get_saved_searches, ConnectionName}, _From, S) ->
+    C = splunkclient_login:get_connection(ConnectionName),
+    case libget_saved_searches(C) of
+        {ok, Results} ->
+            {reply, {ok, search_results, Results}, S};
+        {error, Reason} ->
+            {stop, error, Reason, S};
+        _Else ->
+            {stop, error, "Unknwon error", S}
     end;
 handle_call({oneshot_search, ConnectionName, SearchTerm}, _From, S) ->
     C = splunkclient_login:get_connection(ConnectionName),
@@ -74,8 +100,26 @@ code_change(_OldVersion, State, _Extra) -> {ok, State}.
 %% Internal functions
 %% ============================================================================
 
+libget_indexes(Connection) ->
+    Uri = splunkclient_http:get_base_uri(Connection) ++ "/services/data/indexes/",
+    Params = [],
+    Headers = [{"Authorization", Connection#splunkclient_conn.token}],
+    {ok, ResponseBody} = splunkclient_http:get(Uri, Params, Headers),
+    %{XML, _} = xmerl_scan:string(ResponseBody),
+    %io:fwrite("got xml result: ~s~n", [XML]),
+    {ok, ResponseBody}.
+
 libget_jobs(Connection) ->
-    Uri = splunkclient_http:get_base_uri(Connection) ++ "/services/search/jobs",
+    Uri = splunkclient_http:get_base_uri(Connection) ++ "/services/search/jobs/",
+    Params = [],
+    Headers = [{"Authorization", Connection#splunkclient_conn.token}],
+    {ok, ResponseBody} = splunkclient_http:get(Uri, Params, Headers),
+    %{XML, _} = xmerl_scan:string(ResponseBody),
+    %io:fwrite("got xml result: ~s~n", [XML]),
+    {ok, ResponseBody}.
+
+libget_saved_searches(Connection) ->
+    Uri = splunkclient_http:get_base_uri(Connection) ++ "/services/saved/searches/",
     Params = [],
     Headers = [{"Authorization", Connection#splunkclient_conn.token}],
     {ok, ResponseBody} = splunkclient_http:get(Uri, Params, Headers),
@@ -84,7 +128,7 @@ libget_jobs(Connection) ->
     {ok, ResponseBody}.
 
 liboneshot_search(Connection, SearchTerm) ->
-    Uri = splunkclient_http:get_base_uri(Connection) ++ "/services/search/jobs",
+    Uri = splunkclient_http:get_base_uri(Connection) ++ "/services/search/jobs/",
     Params = [{"exec_mode", "oneshot"}, {"search", "search " ++ SearchTerm}],
     Headers = [{"Authorization", Connection#splunkclient_conn.token}],
     {ok, ResponseBody} = splunkclient_http:post(Uri, Params, Headers),
