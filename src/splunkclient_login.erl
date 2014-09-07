5a21fb6497cb51fb0a2108d6b606f1fa310a6113
Unix1 <unix1@live.com>
2014-09-07 09:15:55 -0700

    remove commented out code

--- src/splunkclient_login.erl
+++ src/splunkclient_login.erl
@@ -10,16 +10,6 @@
 %% Behavior functions
 %% ============================================================================
 
-% login(C, HttpState) ->
-%     Body = [{"username", httpclient_conn:get_user(C)},
-%             {"password", httpclient_conn:get_pass(C)}],
-%     {ok, 200, _ResponseHeaders, ResponseBody} = httpclient_http:post(C,
-%         HttpState, "/services/auth/login", [], [], Body),
-%     {XML, _} = xmerl_scan:string(binary_to_list(ResponseBody)),
-%     [#xmlText{value = SessionKey}] =
-%         xmerl_xpath:string("/response/sessionKey/text()", XML),
-%     {ok, "Splunk " ++ SessionKey}.
-
 login(Conn, HttpState) ->
     Path = <<"/services/auth/login">>,
     Body = [{<<"username">>, httpclient_conn:get_user(Conn)},
@@ -31,19 +21,3 @@ login(Conn, HttpState) ->
     [#xmlText{value = SessionKey}] =
         xmerl_xpath:string("/response/sessionKey/text()", XML),
     {ok, iolist_to_binary([<<"Splunk ">>, list_to_binary(SessionKey)])}.
-
-% login(C) ->
-%     User = httpclient_conn:get_user(C),
-%     Pass = httpclient_conn:get_pass(C),
-%     {ok, 200, _, ResponseBody} =
-%         httpclient_service:request(Conn, {get_indexes, [User, Pass]}),
-%     {XML, _} = xmerl_scan:string(binary_to_list(ResponseBody)),
-%     [#xmlText{value = SessionKey}] =
-%         xmerl_xpath:string("/response/sessionKey/text()", XML),
-%     {ok, "Splunk " ++ SessionKey}.
-% 
-% get_request(login, [User, Pass], _Token) ->
-%     Path = <<"/services/auth/login">>,
-%     Body = [{"username", User},
-%             {"password", Pass}],
-%     {ok, httpclient_req:new(post, Headers, Path, Body)}.
